0482c0ee4d88e921a37afd1f9b65ff440611bb8a
JoeG21 <jcggomez@live.com>
2020-08-20 12:03:37 -0500

    2 forms in one page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   resources :orders,only: [:new, :show, :create]
   resources :ordered_items
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
-  resources :comments, only: [:show]
+  resources :comments, only: [:show, :new, :create]
   resources :shopping_carts, only: [:show]
   resources :selected_items, only: [:show, :index, :create, :update, :destroy]
   resources :categories, only: [:index, :new, :show, :create]
   resources :items, only: [:index, :show, :new, :create]
+
 
   get '/login', to: 'auth#login'
   post '/login', to: 'auth#verify'
